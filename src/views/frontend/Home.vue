44d4a7d377b21090793dc334939482ce7b44bdf8
RookieItDev <peerapong.it-st@rmutsb.ac.th>
2022-07-26 09:13:28 +0700

    Create Modal

--- src/views/frontend/Home.vue
+++ src/views/frontend/Home.vue
@@ -8,8 +8,28 @@
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae congue tortor.</p>
                 </div>
                 <div class="w-full p-4 lg:w-auto">
-                    <a href="#" class="inline-block px-6 py-2 text-white bg-green-600 border-2 border-green-600 rounded-md hover:text-green-600 hover:bg-green-100">Modal</a>
+                    <a href="#" @click="toggleModal = !toggleModal" class="inline-block px-6 py-2 text-white bg-green-600 border-2 border-green-600 rounded-xl hover:text-green-600 hover:bg-green-100 ">Modal</a>
                 </div>
+                <div class="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto" v-if="toggleModal">
+    <div class="relative w-auto max-w-2xl mx-auto">
+        <div class="flex flex-col w-full bg-white rounded shadow-2xl">
+            <span class="p-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat odio neque, corporis beatae dolores natus dolorum pariatur aperiam vel quidem cupiditate voluptas hic molestiae veniam placeat facilis. Rem, facilis autem.</span>
+     <input type="datetime-local" name="" id="">
+     <select name="" id="">
+         
+     </select>
+      inpu
+      <div class="flex flex-row ">
+        <button @click="toggleModal=false" class="w-3/12 py-2 m-auto mt-1 mb-3 text-white bg-green-500 rounded-xl">Close</button>
+        <button type="submit" @click="Update()" class="w-3/12 py-2 m-auto mt-1 mb-3 text-white bg-green-500 rounded-xl">Save change</button>
+      </div>
+       
+        </div>
+    </div>
+</div>
+<div v-if="toggleModal" class="fixed overflow-x-hidden overflow-y-auto">
+</div>
+<div v-if="toggleModal" class="absolute inset-0 z-40 bg-black opacity-25 "></div>
             </div>
             <div class="flex flex-wrap">
                 <div class="w-full p-4 xl:w-3/12 sm:w-6/12">
@@ -38,3 +58,20 @@
         </div>
     </section>
 </template>
+ <script>
+ export default {
+     name: "modal",
+     data(){
+            return {
+                toggleModal: false
+            }
+     },
+     methods:{
+         Update(){
+
+         }
+     }
+
+
+ }
+ </script>
